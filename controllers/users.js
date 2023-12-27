186b440b54e225bb6d155219931354f82b8456c0
Carl <carlctha1@gmail.com>
2023-12-28 00:47:52 +0100

    Added middleware errorhandling

--- controllers/users.js
+++ controllers/users.js
@@ -7,29 +7,33 @@ usersRouter.get("/", async (req, res) => {
     res.status(200).json(users);
 });
 
-usersRouter.post("/", async (req, res) => {
-    const { username, name, password } = req.body;
+usersRouter.post("/", async (req, res, next) => {
+    try {
+        const { username, name, password } = req.body;
 
-    if (username.length < 3) {
-        res.status(400).json({ error: "Username must be at least 3 characthers" });
+        if (username.length < 3) {
+            res.status(400).json({ error: "Username must be at least 3 characthers" });
+        };
+
+        if (password.length < 3) {
+            res.status(400).json({ error: "Password must be at least 3 characthers" });
+        };
+
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password, saltRounds);
+
+        const user = new User({
+            username,
+            name,
+            passwordHash
+        });
+
+        const savedUser = await user.save();
+
+        res.status(201).json(savedUser);
+    } catch (error) {
+        next(error);
     };
-
-    if (password.length < 3) {
-        res.status(400).json({ error: "Password must be at least 3 characthers" });
-    };
-
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds);
-
-    const user = new User({
-        username,
-        name,
-        passwordHash
-    });
-
-    const savedUser = await user.save();
-
-    res.status(201).json(savedUser);
 });
 
 module.exports = usersRouter;
